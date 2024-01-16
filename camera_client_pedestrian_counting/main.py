f173dd36cff95670066492daa35d2f485675946e
josericardopenase <josericardopenase@gmail.com>
2024-01-16 13:34:20 +0000

    adsfasdf

--- camera_client_pedestrian_counting/main.py
+++ camera_client_pedestrian_counting/main.py
@@ -1,15 +1,14 @@
 from .control import PersonCounterController
 from .camera import CameraServer
-import multiprocessing
+import threading
 import cv2
-import os
+import queue
 
-def send_frames(frame_queue, exit_flag, camera_server_info):
+def send_frames(frame_queue, exit_event, camera_server_info):
     camera = CameraServer(ip=camera_server_info[0], port=camera_server_info[1])
     camera.start()
-    print(frame_queue)
 
-    while not exit_flag.value:
+    while not exit_event.is_set():
         while not frame_queue.empty():
             if frame_queue.qsize() > 10:
                 frame_queue.get_nowait()
@@ -22,7 +21,7 @@ def send_frames(frame_queue, exit_flag, camera_server_info):
                 camera.send_frame(frame)
             except Exception as e:
                 print(f"Failed to send frame: {e}")
-                exit_flag.value = 1
+                exit_event.set()
 
     camera.stop()
 
@@ -31,14 +30,13 @@ def main():
     control = PersonCounterController(width=1020, height=600, limit=20, max_age=10, model="./yolo/yolov8n-seg.pt")
 
     # Initialize frame queue and exit flag
-    frame_queue = multiprocessing.Queue()
-    exit_flag = multiprocessing.Value('i', 0)
+    frame_queue = queue.Queue()
+    exit_event = threading.Event()
 
-    # Start the frame sending process
+    # Start the frame sending thread
     camera_server_info = ("127.0.0.1", 5006)
-    sending_process = multiprocessing.Process(target=send_frames, args=(frame_queue, exit_flag, camera_server_info))
-    sending_process.start()
-
+    sending_thread = threading.Thread(target=send_frames, args=(frame_queue, exit_event, camera_server_info))
+    sending_thread.start()
     # Main loop to capture and process frames
     cap = cv2.VideoCapture(0)
     try:
@@ -48,12 +46,12 @@ def main():
             frame = control.read(frame)
             frame_queue.put(frame)
             cv2.imshow("Camera", frame)
-            if cv2.waitKey(20) == 27: break
+            if cv2.waitKey(20) == 27: break  # Escape key pressed
     finally:
-        exit_flag.value = 1
-        sending_process.join()
+        exit_event.set()
+        sending_thread.join()
         cap.release()
         cv2.destroyAllWindows()
 
-if __name__ == '__main__':
+if __name__ == 'main':
     main()
