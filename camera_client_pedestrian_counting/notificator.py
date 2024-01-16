126981662cc66bf728378e5400e1b0365e991498
josericardopenase <josericardopenase@gmail.com>
2024-01-16 16:09:42 +0000

    adsfadsf

--- camera_client_pedestrian_counting/notificator.py
+++ camera_client_pedestrian_counting/notificator.py
@@ -1,12 +1,10 @@
 
 import requests
 
+ip = "192.168.8.102"
+
 def notify_client_entered():
-    pass
-    #requests.post('http://172.20.10.2:5000/client_entered/')
+    requests.post('http://'+ip+'/client_entered/')
 
 def notify_client_leave():
-    pass
-   #requests.post('http://172.20.10.2:5000/client_exited/')
-
-#notify_client_leave()
+    requests.post('http://'+ ip + '/client_exited/')
